4b5eb2afa71f5bd5b670da18509381303c7b9d18
Austin <augustineumeagudosi@gmail.com>
2022-02-28 00:54:47 +0100

    added tests for the enncode endpoint

---
+++ test/integration/url.test.js
@@ -0,0 +1,68 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import 'dotenv/config';
+import app from '../../index';
+import * as urlFixtures from '../fixtures/url';
+import {Url} from "../../src/models";
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+const baseUrl = '/api/v1/urls';
+
+describe('Url Module', () => {
+
+  before( async () => {
+    await Url.deleteMany();
+  });
+
+  // encode
+  it('should create a short url', (done) => {
+    chai.request(app)
+      .post(`${baseUrl}/encode`)
+      .send(urlFixtures.longUrl)
+      .end((err, res) => {
+        const { message, status, data:{shortUrl} } = res.body;
+        process.env.shortUrl = shortUrl;
+        expect(status).to.equal('success');
+        expect(message).to.equal('Url shortened successfully!');
+        done();
+      });
+  });
+
+  it('should fail when an invalid url formate is supplied', (done) => {
+    chai.request(app)
+      .post(`${baseUrl}/encode`)
+      .send(urlFixtures.badLongUrl)
+      .end((err, res) => {
+        const { message, status } = res.body;
+        expect(status).to.equal('error');
+        expect(message).to.equal("Url should be in this format 'http://foo.bar' or 'https://foo.bar'");
+        done();
+      });
+  });
+
+  it('should fail when no long url is supplied', (done) => {
+    chai.request(app)
+      .post(`${baseUrl}/encode`)
+      .end((err, res) => {
+        const { message, status } = res.body;
+        expect(status).to.equal('error');
+        expect(message).to.equal("longUrl is required");
+        done();
+      });
+  });
+
+  it('should fail when the supplied long url has already been shortened', (done) => {
+    chai.request(app)
+      .post(`${baseUrl}/encode`)
+      .send(urlFixtures.longUrl)
+      .end((err, res) => {
+        const { message, status } = res.body;
+        expect(status).to.equal('error');
+        expect(message).to.equal(`${urlFixtures.longUrl.longUrl} has already been shortened before`);
+        done();
+      });
+  });
+
+});
